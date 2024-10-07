f11163913096a5b597c5e0b3fd762b97ace3020b
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2024-10-07 21:35:25 +0300

    update user model

--- user-module/user-model.js
+++ user-module/user-model.js
@@ -2,7 +2,30 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({});
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["employee", "manager"],
+      default: "employee",
+    },
+  },
+  { timestamp: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
